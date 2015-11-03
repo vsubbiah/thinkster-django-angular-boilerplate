308a3c26c95961cdfa6b71b1498d764b4ce1d0e6
Venkat Subbiah <v.subbiah@gmail.com>
2015-11-02 18:40:22 -0800

    Upto checkpoint of registering new user.

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,14 @@
 angular
   .module('thinkster', []);
+  .run(run);
+
+run.$inject = ['$http'];
+
+/**
+* @name run
+* @desc Update xsrf $http headers to align with Django's defaults
+*/
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import patterns, url
-
 from thinkster_django_angular_boilerplate.views import IndexView
 
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
-    '',
+     '',
+    # ... URLs
+    url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
